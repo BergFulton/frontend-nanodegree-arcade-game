306558e32b2d83518d9726aaac5e0ce383f4a011
BergFulton <BergFulton@users.noreply.github.com>
2016-02-22 19:59:10 -0500

    Fix: Player & Enemy collision, additional commenting.

--- js/app.js
+++ js/app.js
@@ -1,7 +1,9 @@
-// Enemies our player must avoid
+
+// ENEMY CLASS
 var Enemy = function(x, y) {
     this.x = x;
     this.y = y;
+
 
     //Save the original position as startX and startY 
     //so bugs stream constantly by resetting to their original position. 
@@ -12,32 +14,41 @@ var Enemy = function(x, y) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
+    // Sets the avatar for the enemy bugs.
     this.sprite = 'images/enemy-bug.png';
+
+    // This is the actual height/width of the sprites. 
+    // It matters for collision detection. 
+    this.height = 77;
+    this.width = 101;
 };
 
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
+// Update the enemy's position. 
+
 Enemy.prototype.update = function(dt) {
-    //this line with help from 
+    
+    // This line with help from 
     // http://tonirib.github.io/frontend-nanodegree-arcade-game/
     this.x = this.x + 120 * dt * (this.velocity);
 
-    //Collisions
-    if (this.y == player.y && (this.x > player.x - 50 && this.x < player.x + 50)) 
-        {console.log("collision!");
-    };
+    // Collisions
+    // This code with help from https://www.youtube.com/watch?v=rqEJ7WiLWdo
 
-    //When the bug goes off the screen- further than 650
-    //on the x axis, it resets.
+    if ( (this.x + this.width >= player.x && this.x <= player.x + player.width) 
+        && (this.y >= player.y && this.y <= player.y + player.height) ) {
+        player.reset();
+        alert('Oh noes! You\'ve died!');
+    }
+
+    // When the bug goes off the screen- further than 650
+    // on the x axis, it resets.
     if (this.x > 650){
         this.reset();
     }
 
 };
 
-// Draw the enemy on the screen, required method for game
+// Draw the enemy on the screen.
 Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
@@ -49,22 +60,25 @@ Enemy.prototype.reset = function () {
     this.sprite = 'images/enemy-bug.png';
 }
 
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
+// PLAYER CLASS 
 
 var Player = function(x, y) {
 
     this.x = x;
     this.y = y;
 
-    //Saves the original start position for use in reset. 
+    // Saves the original start position for use in player.reset. 
     this.startX = x;
     this.startY = y;
 
 
-    //This sets the player avatar.
+    // This sets the player avatar.
     this.sprite = 'images/char-cat-girl.png';
+
+    // This is the actual height/width of the sprites. 
+    // It matters for collision detection. 
+    this.height = 81;
+    this.width = 68;
 };
 
 Player.prototype.update = function() { 
@@ -74,6 +88,8 @@ Player.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);  
 };
 
+// Start the player back at its beginning position. 
+
 Player.prototype.reset = function() {
     this.x = this.startX;
     this.y= this.startY;
@@ -81,48 +97,51 @@ Player.prototype.reset = function() {
 }
 
 Player.prototype.handleInput = function(allowedKeys) {
-   //This sets the boundaries for the player. 
+
+   // This sets the boundaries for the player. 
+   
    switch (allowedKeys) {
+        //Left boundary.
         case 'left': if (this.x > 0){
             this.x = this.x - 101;}
         break;
-                
+        
+        // Right boundary.
         case 'right': if (this.x < 404) {
             this.x = this.x + 101}
         break;
         
-        //If the player's y position is > 50, it means the player
-        //is not in the water portion, and therefore hasn't won.
+        // If the player's y position is > 50, it means the player
+        // is not in the very top water portion, and therefore hasn't won.
         case 'up':
             if (this.y > 50) {
                 this.y = this.y - 70;
             } 
-        //If the player's y position is < 35, it means the player
-        //has won and can reset. 
+        // If the player's y position is < 35, it means the player
+        // is in the water portion, and has won and can reset. 
             else {
-                alert("w00t!");
+                alert("w00t! You win!");
                 player.reset();
             }
         break;
 
+        // Bottom boundary.
         case 'down': if(this.y < 375){
             this.y = this.y + 75;}
         break;
    }
 };
 
-// Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
+// Instantient Enemies and Player.
 
 var allEnemies = [];
 
-//Three horizontal positions that are possible for the bugs 
-//to move in. These are the gray blocks. 
+// Three positions that are possible for the bugs 
+// to move in. These are the gray blocks. 
 
 var vertPos = [220, 140, 60];
 
-//Create different enemies with different properties. 
+// Create 3 different enemies with different properties. 
 for (var i = 0; i < 3; i++) {
     var x = Math.floor((Math.random() * 5 + 1));
 
@@ -130,13 +149,13 @@ for (var i = 0; i < 3; i++) {
 
     var enemy = new Enemy(x, y);
 
-//Now push those bugs in to the allEnemies array!
+// Now push those bugs in to the allEnemies array!
     allEnemies.push(enemy);
 }
 
 
-//Instantiates the new player in the center bottom tile.
-//When player.reset is called, it goes back to this position. 
+// Instantiates the new player in the center bottom tile.
+// When player.reset is called, it goes back to this position. 
 
 var player = new Player (202, 420);
 
